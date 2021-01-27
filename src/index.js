e7fac397e7757a5326839940577dc12618770fce
yanghm <code004@gmail.com>
2021-01-27 17:40:34 +0900

    6.7 redux 할일 목록 구현

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import rootReducer from "./modules";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension"; // 리덕스 개발자 도구
 
-const store = createStore(rootReducer); // 스토어를 만듭니다.
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
+// composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
 
 ReactDOM.render(
   <Provider store={store}>
